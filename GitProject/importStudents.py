e817c0710f4b4cfc425a59d5e3b9140d09ad95b6
Lukas249 <lukaszradecki6@gmail.com>
2024-10-26 00:34:20 +0200

    feat: import students from txt and csv files

---
+++ GitProject/importStudents.py
@@ -0,0 +1,48 @@
+class ImportStudents:
+    student_details_structure = ["Name", "Surname", "ID"]
+
+    @staticmethod
+    def csv(path):
+
+        student_details_structure = ImportStudents.student_details_structure
+
+        students = []
+
+        file = open(path, "r")
+
+        for line in file:
+            line = line.rstrip()
+
+            student_details_dict = {}
+
+            student_details = line.split(";")
+
+            for i in range(len(student_details_structure)):
+                student_details_dict[student_details_structure[i]] = student_details[i]
+
+            students.append(student_details_dict)
+
+        return students
+
+    @staticmethod
+    def txt(path):
+
+        student_details_structure = ImportStudents.student_details_structure
+
+        students = []
+
+        file = open(path, "r")
+
+        for line in file:
+            line = line.rstrip()
+
+            student_details_dict = {}
+
+            student_details = [j for i in line.split(" - ") for j in i.split(" ")]
+
+            for i in range(len(student_details_structure)):
+                student_details_dict[student_details_structure[i]] = student_details[i]
+
+            students.append(student_details_dict)
+
+        return students
